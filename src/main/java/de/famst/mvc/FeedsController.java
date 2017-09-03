f3f2b1084cf50a26d0db5821faf3660435871513
Jens Stockhausen <jens@famst.de>
2017-09-03 15:19:21 +0200

    adding a summary

--- src/main/java/de/famst/mvc/FeedsController.java
+++ src/main/java/de/famst/mvc/FeedsController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class FeedsController
@@ -35,6 +38,35 @@ public class FeedsController
         model.addAttribute("date", date);
         model.addAttribute("feeds", feedByDate);
 
+        Map<String, Map<String, String>> summaries = new HashMap<>();
+
+        for (int c = 2; c < 6; c++)
+        {
+            Map<String, String> summary = new HashMap<>();
+
+            List<FeedEntity> feedByDateChannel = feedRepository.findByDateAndChannelOrderByTimeAsc(Date.valueOf(date), c);
+
+            if (feedByDateChannel.size() != 0)
+            {
+                String first = feedByDateChannel.get(0).getTime().toString();
+                String last = feedByDateChannel.get(feedByDateChannel.size() - 1).getTime().toString();
+                String count = String.format("%3d", feedByDateChannel.size());
+
+                summary.put("first", first);
+                summary.put("last", last);
+                summary.put("count", count);
+            } else
+            {
+                summary.put("first", "");
+                summary.put("last", "");
+                summary.put("count", "0");
+            }
+
+            summaries.put(String.valueOf(c), summary);
+        }
+
+        model.addAttribute("summaries", summaries);
+
         return "daysummary";
     }
 
@@ -51,9 +83,6 @@ public class FeedsController
 
         return "daysummary";
     }
-
-
-
 
 
 }
--- src/test/java/TestMVC.java
+++ src/test/java/TestMVC.java
@@ -16,9 +16,11 @@ import org.springframework.web.servlet.ModelAndView;
 import java.sql.Date;
 import java.sql.Time;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
@@ -55,11 +57,42 @@ public class TestMVC
         assertThat(modelAndView.getViewName(), is("daysummary"));
         assertThat(modelAndView.getModel().get("date"), is("2017-08-05"));
 
-        List<FeedEntity> feeds = (List<FeedEntity>)modelAndView.getModel().get("feeds");
+        List<FeedEntity> feeds = (List<FeedEntity>) modelAndView.getModel().get("feeds");
 
         assertThat(feeds.size(), is(5));
     }
 
+
+    @Test
+    public void summariesAreAdded() throws Exception
+    {
+        feedRepository.save(new FeedEntity(
+            Date.valueOf("2017-08-05"), Time.valueOf("19:33:45"), 7.5, 4));
+        feedRepository.save(new FeedEntity(
+            Date.valueOf("2017-08-05"), Time.valueOf("20:33:45"), 7.5, 2));
+        feedRepository.save(new FeedEntity(
+            Date.valueOf("2017-08-05"), Time.valueOf("21:33:45"), 7.5, 3));
+        feedRepository.save(new FeedEntity(
+            Date.valueOf("2017-08-05"), Time.valueOf("22:33:45"), 7.5, 4));
+        feedRepository.save(new FeedEntity(
+            Date.valueOf("2017-08-05"), Time.valueOf("23:33:45"), 7.5, 2));
+
+
+        MvcResult mvcResult = mockMvc.perform(get("/dailyfeeds/2017-08-05")).andReturn();
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+
+        Object summariesObject = modelAndView.getModel().get("summaries");
+
+        assertThat(summariesObject, is(notNullValue()));
+
+        // check size and type
+        assertThat(((Map<String, Map<String, String>>) summariesObject).size(), is(4));
+        Map<String, Map<String, String>> summaries = (Map<String, Map<String, String>>) summariesObject;
+
+        assertThat(summaries.get("2").get("first"), is("20:33:45"));
+        assertThat(summaries.get("2").get("last"), is("23:33:45"));
+        assertThat(summaries.get("2").get("count"), is("  2"));
+    }
 
 
 }

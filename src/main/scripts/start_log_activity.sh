ce51da6d1c54e6e0116e438474ddbcb4a89862f2
Jens Stockhausen <jens@famst.de>
2018-09-12 19:41:26 +0200

    inverse GPIO, log to file

--- src/main/scripts/start_log_activity.sh
+++ src/main/scripts/start_log_activity.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd "${dirname "$0"}"
 
+printf "running in $PWD\n"
+
 mkdir ./log
 
 logger CATWATCHER: starting activity logger
 
-python log_activity.py >> ./log/activity.log
+python log_activity.py
+

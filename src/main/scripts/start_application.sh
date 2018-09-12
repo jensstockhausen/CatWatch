eca1cdc0cd5783a0ccc00df9e0fe6c2b13c5aead
Jens Stockhausen <jens@famst.de>
2018-09-12 20:21:07 +0200

    up and running

--- src/main/scripts/start_application.sh
+++ src/main/scripts/start_application.sh
@@ -5,5 +5,7 @@ logger CATWATCHER: starting application
 
 cd ./application
 
-java -Dlogfilepath=./../log/activity.log -jar CatWatch-1.0.0.jar
+java -Dlogfilepath=./../log/activity.log -jar CatWatch-1.0.0.jar >> catwatch.log
+
+
 
